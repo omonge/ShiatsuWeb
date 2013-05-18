a2c8c8fb11bd60dfc8487fae62b720cb8ef03469
omonge <oscarmongev@gmail.com>
2013-05-17 21:51:07 -0600

    Bodega, Recargo

---
+++ ShiatsuWeb/LogicaNegocio/Recargo.vb
@@ -0,0 +1,62 @@
+﻿Public Class Recargo
+
+    Dim id As Integer
+    Dim descripcion As String
+    Dim estado As String
+    Dim usuario As String
+    Dim fmodifica As DateTime
+
+
+    Sub New()
+        Me.fmodifica = Nothing
+        Me.usuario = vbNull
+        Me.id = vbNull
+        Me.estado = vbNull
+        Me.descripcion = vbNull
+    End Sub
+
+    Public Property metFmodifica() As DateTime
+        Get
+            Return Me.fmodifica
+        End Get
+        Set(ByVal value As DateTime)
+            Me.fmodifica = value
+        End Set
+    End Property
+
+    Public Property metUsuario() As String
+        Get
+            Return Me.usuario
+        End Get
+        Set(ByVal value As String)
+            Me.usuario = value
+        End Set
+    End Property
+
+    Public Property metDescripcion() As String
+        Get
+            Return Me.descripcion
+        End Get
+        Set(ByVal value As String)
+            Me.descripcion = value
+        End Set
+    End Property
+
+    Public Property metEstado() As String
+        Get
+            Return Me.estado
+        End Get
+        Set(ByVal value As String)
+            Me.estado = value
+        End Set
+    End Property
+
+    Public Property metId() As Integer
+        Get
+            Return Me.id
+        End Get
+        Set(ByVal value As Integer)
+            Me.id = value
+        End Set
+    End Property
+End Class
